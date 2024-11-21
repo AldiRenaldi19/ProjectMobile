e6651cd621db6e7d5be4647ac3dc6f414e2f16a6
aldi1911 <aldireynaldi1911@gmail.com>
2024-11-21 22:05:03 +0700

    Menambahkan Logika untuk Berpindah ke Login Screen

--- app/src/main/java/com/example/mindnotes/MainActivity.kt
+++ app/src/main/java/com/example/mindnotes/MainActivity.kt
@@ -1,32 +1,26 @@
 package com.example.mindnotes
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var menujuLoginScreen: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         menujuLoginScreen = findViewById(R.id.menujuLoginScreen)
 
         menujuLoginScreen.setOnClickListener {
-            val MULAI = menujuLoginScreen.text.toString()
-        }
-            Toast.makeText(this, "Selamat Datang", Toast.LENGTH_SHORT)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            // Intent Menuju Login Screen
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
