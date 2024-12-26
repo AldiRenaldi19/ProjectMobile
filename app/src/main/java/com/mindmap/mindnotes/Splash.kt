5d2fb9ceb4c2c46e34da616acf432e712397bd4f
aldi1911 <aldireynaldi1911@gmail.com>
2024-12-26 17:21:00 +0700

    edit layout dan file

--- app/src/main/java/com/mindmap/mindnotes/Splash.kt
+++ app/src/main/java/com/mindmap/mindnotes/Splash.kt
@@ -3,25 +3,25 @@ package com.mindmap.mindnotes
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.WindowManager.LayoutParams.*
-import androidx.activity.enableEdgeToEdge
+import android.os.Looper
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        //menghapus status bar
-        window.setFlags(
-            FLAG_FULLSCREEN,
-            FLAG_FULLSCREEN
-        )
-        //menggunakan postDelayed (Runnable, Time)
-        Handler().postDelayed({
 
+        //menghapus status bar
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        //menggunakan postDelayed (Runnable, Time)
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainScreenActivity::class.java)
             startActivity(intent)
             finish()
